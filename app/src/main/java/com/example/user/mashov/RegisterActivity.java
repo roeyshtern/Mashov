1674af8f2005994f157235f9027266e3084bcb2f
RoeyShtern <roeyshtern@gmail.com>
2017-12-10 17:00:30 +0200

    10.12.17

---
+++ app/src/main/java/com/example/user/mashov/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.user.mashov;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by User on 12/10/17.
+ */
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_layout);
+    }
+}
